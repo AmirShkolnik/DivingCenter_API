8a31aa2bfdc365e99a49c4f8645da1d40c9fe380
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-06-18 05:59:44 +0000

    likes/views - create generic views

---
+++ likes/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics, permissions
+from pp5_api.permissions import IsOwnerOrReadOnly
+from likes.models import Like
+from likes.serializers import LikeSerializer
+
+
+class LikeList(generics.ListCreateAPIView):
+    """
+    List likes or create a like if logged in.
+    """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    serializer_class = LikeSerializer
+    queryset = Like.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
