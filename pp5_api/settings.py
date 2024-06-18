46df3313b71abc8555e197150487da14ac003dbb
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-06-18 19:46:13 +0000

    settings - add localhost to allowed hosts

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -62,7 +62,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['http://localhost:8000', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
 
 
 # Application definition
