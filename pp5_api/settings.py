4ca8e447579168b25c0fb27c8f0c8f05f3f90865
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-06-18 16:11:18 +0000

    setting - fix error - change drf to pp5

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -38,7 +38,7 @@ JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'pp5_api.serializers.CurrentUserSerializer'
 }
 
 # Quick-start development settings - unsuitable for production
