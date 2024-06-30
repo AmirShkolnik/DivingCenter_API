8558d489a1caf08f562fb1fbc122cc48c1487482
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-06-30 16:44:26 +0000

    add def get_tinymce_config

--- courses/admin.py
+++ courses/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.db import models
 from django.utils.safestring import mark_safe
 from .models import Course, Review
-from tinymce.models import HTMLField
 from tinymce.widgets import TinyMCE
 
 class ReviewInline(admin.TabularInline):
@@ -12,15 +11,34 @@ class ReviewInline(admin.TabularInline):
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'course_type', 'average_rating', 'created_at', 'updated_at')
+    list_display = ('title', 'slug', 'course_type', 'get_average_rating', 'created_at', 'updated_at')
     list_filter = ('course_type', 'created_at')
     search_fields = ('title', 'slug', 'description')
     prepopulated_fields = {'slug': ('title',)}
     inlines = [ReviewInline]
     readonly_fields = ('image_preview',)
-    formfield_overrides = {
-        models.TextField: {'widget': TinyMCE()},
-    }
+
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'description':
+            return db_field.formfield(widget=TinyMCE(
+                attrs={'cols': 80, 'rows': 30},
+                mce_attrs=self.get_tinymce_config()
+            ))
+        return super().formfield_for_dbfield(db_field, **kwargs)
+
+    def get_tinymce_config(self):
+        return {
+            'theme': 'silver',
+            'height': 500,
+            'plugins': [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table paste code help wordcount'
+            ],
+            'toolbar': 'undo redo | formatselect | bold italic backcolor | \
+                        alignleft aligncenter alignright alignjustify | \
+                        bullist numlist outdent indent | removeformat | help'
+        }
 
     def image_preview(self, obj):
         if obj.image:
@@ -28,12 +46,12 @@ class CourseAdmin(admin.ModelAdmin):
         return "No Image"
     image_preview.short_description = 'Image Preview'
 
-    def average_rating(self, obj):
+    def get_average_rating(self, obj):
         reviews = obj.reviews.all()
         if reviews:
             return sum(review.rating for review in reviews) / len(reviews)
         return 0
-    average_rating.short_description = 'Avg Rating'
+    get_average_rating.short_description = 'Avg Rating'
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
