2be7528cdd58199fbf3c724f00c8a6e8a234cca9
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-06-30 09:12:55 +0000

    Serializers - fix frontend error image not desplay- add imgae full link to return to the frontend

--- courses/serializers.py
+++ courses/serializers.py
@@ -9,9 +9,15 @@ class ReviewSerializer(serializers.ModelSerializer):
         fields = ['id', 'user', 'course', 'content', 'rating', 'created_at', 'updated_at']
         read_only_fields = ['id', 'user', 'created_at', 'updated_at']
 
+    def validate(self, data):
+        if data['rating'] < 1 or data['rating'] > 5:
+            raise serializers.ValidationError("Rating must be between 1 and 5")
+        return data
+
 class CourseSerializer(serializers.ModelSerializer):
     reviews = ReviewSerializer(many=True, read_only=True)
     average_rating = serializers.SerializerMethodField()
+    image = serializers.SerializerMethodField()
 
     class Meta:
         model = Course
@@ -23,3 +29,8 @@ class CourseSerializer(serializers.ModelSerializer):
         if reviews:
             return sum(review.rating for review in reviews) / len(reviews)
         return 0
+
+    def get_image(self, obj):
+        if obj.image:
+            return obj.image.url
+        return None
