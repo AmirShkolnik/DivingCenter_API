a17c0bc9509df4104c291efbf5c6e8011cf0701c
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-07-01 05:38:04 +0000

    serializers - remove '' srounding the $ sign

--- courses/serializers.py
+++ courses/serializers.py
@@ -37,4 +37,4 @@ class CourseSerializer(serializers.ModelSerializer):
         return None
 
     def get_price_display(self, obj):
-        return f"{obj.price} '$'"
+        return f"{obj.price} $"
