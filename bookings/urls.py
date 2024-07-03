f017de4bd10573bf9cbc616d2d321803f99f57e6
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-07-03 15:20:43 +0000

    bookings/views - fix issue that any authenticated user to view and modify all bookings modifying the BookingViewSet to only allow users to see and modify their own bookings add custom permission class IsOwnerOrReadOnly

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import BookingViewSet, DivingCourseViewSet
 
 router = DefaultRouter()
-router.register(r'bookings', BookingViewSet)
+router.register(r'bookings', BookingViewSet, basename='booking')
 router.register(r'diving-courses', DivingCourseViewSet)
 
 urlpatterns = [
--- bookings/views.py
+++ bookings/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, permissions
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Booking, DivingCourse
@@ -8,10 +8,29 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the booking.
+        return obj.user == request.user
+
 class BookingViewSet(viewsets.ModelViewSet):
     serializer_class = BookingSerializer
-    permission_classes = [IsAuthenticated]
-    queryset = Booking.objects.all()
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the bookings
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return Booking.objects.filter(user=user)
 
     def create(self, request, *args, **kwargs):
         logger.info(f"Attempting to create booking for user {request.user.username}")
