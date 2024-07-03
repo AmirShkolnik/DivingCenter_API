65258f9f7dde56992d12bed551b74b81ec1e6cf4
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-07-03 10:24:26 +0000

    contacts/views - Add a get method to the ContactView class to handle GET requests Add IsAuthenticated permission class to ensure only authenticated users can access these views Fix the delete_contact function to only allow deletion of messages associated with the authenticated user's email contacts/models - add a reference to the logged in user

--- contactus/models.py
+++ contactus/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='contacts', null=True, blank=True)
     name = models.CharField(max_length=100)
     email = models.EmailField()
     subject = models.CharField(max_length=200)
--- contactus/views.py
+++ contactus/views.py
@@ -1,11 +1,19 @@
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from .serializers import ContactSerializer
 from .models import Contact
 
 class ContactView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        contacts = Contact.objects.filter(email=request.user.email)
+        serializer = ContactSerializer(contacts, many=True)
+        return Response(serializer.data)
+
     def post(self, request):
         serializer = ContactSerializer(data=request.data)
         if serializer.is_valid():
@@ -17,9 +25,10 @@ class ContactView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
 def delete_contact(request, pk):
     try:
-        contact = Contact.objects.get(pk=pk)
+        contact = Contact.objects.get(pk=pk, email=request.user.email)
         contact.delete()
         return Response({"message": "Contact deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
     except Contact.DoesNotExist:
