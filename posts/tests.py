f583a2e0b067e38aa07f1a548ef86441356f6a28
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-07-20 11:15:50 +0000

    posts - remove empty lines nad shorten long lines for all .py files add python linter testing table and results images

--- posts/tests.py
+++ posts/tests.py
@@ -4,18 +4,21 @@ from .models import Post
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
+
 class PostListViewTests(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create_user(username='adam', password='pass')
         self.url = reverse('post-list')
-    
+
     def authenticate(self):
         self.client.login(username='adam', password='pass')
         self.client.force_authenticate(user=self.user)
 
     def test_can_list_posts(self):
-        Post.objects.create(owner=self.user, title='a title', content='some content')
+        Post.objects.create(
+            owner=self.user, title='a title', content='some content'
+        )
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 1)
@@ -23,13 +26,17 @@ class PostListViewTests(APITestCase):
 
     def test_logged_in_user_can_create_post(self):
         self.authenticate()
-        response = self.client.post(self.url, {'title': 'a title', 'content': 'some content'})
+        response = self.client.post(
+            self.url, {'title': 'a title', 'content': 'some content'}
+        )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 1)
         self.assertEqual(Post.objects.get().title, 'a title')
 
     def test_user_not_logged_in_cant_create_post(self):
-        response = self.client.post(self.url, {'title': 'a title', 'content': 'some content'})
+        response = self.client.post(
+            self.url, {'title': 'a title', 'content': 'some content'}
+        )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -37,12 +44,21 @@ class PostDetailViewTests(APITestCase):
     def setUp(self):
         self.client = APIClient()
         self.adam = User.objects.create_user(username='adam', password='pass')
-        self.brian = User.objects.create_user(username='brian', password='pass')
-        self.adam_post = Post.objects.create(owner=self.adam, title='a title', content='adams content')
-        self.brian_post = Post.objects.create(owner=self.brian, title='another title', content='brians content')
-        self.detail_url_adam = reverse('post-detail', kwargs={'pk': self.adam_post.pk})
-        self.detail_url_brian = reverse('post-detail', kwargs={'pk': self.brian_post.pk})
-    
+        self.brian = User.objects.create_user(username='brian',
+                                              password='pass')
+        self.adam_post = Post.objects.create(
+            owner=self.adam, title='a title', content='adams content'
+        )
+        self.brian_post = Post.objects.create(
+            owner=self.brian, title='another title', content='brians content'
+        )
+        self.detail_url_adam = reverse(
+            'post-detail', kwargs={'pk': self.adam_post.pk}
+        )
+        self.detail_url_brian = reverse(
+            'post-detail', kwargs={'pk': self.brian_post.pk}
+        )
+
     def authenticate(self, user):
         self.client.login(username=user.username, password='pass')
         self.client.force_authenticate(user=user)
@@ -59,12 +75,18 @@ class PostDetailViewTests(APITestCase):
 
     def test_user_can_update_own_post(self):
         self.authenticate(self.adam)
-        response = self.client.put(self.detail_url_adam, {'title': 'a new title', 'content': 'updated content'})
+        response = self.client.put(
+            self.detail_url_adam,
+            {'title': 'a new title', 'content': 'updated content'}
+        )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.adam_post.refresh_from_db()
         self.assertEqual(self.adam_post.title, 'a new title')
 
     def test_user_cant_update_another_users_post(self):
         self.authenticate(self.adam)
-        response = self.client.put(self.detail_url_brian, {'title': 'a new title', 'content': 'updated content'})
+        response = self.client.put(
+            self.detail_url_brian,
+            {'title': 'a new title', 'content': 'updated content'}
+        )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
