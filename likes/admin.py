415040d5b9a59c94dbdd17167be61bd8f302f297
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-07-30 12:24:51 +0000

    Add likes model to Django admin for management

--- likes/admin.py
+++ likes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Like
 
-# Register your models here.
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'post', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('owner__username', 'post__title')
+    date_hierarchy = 'created_at'
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('owner', 'post')
