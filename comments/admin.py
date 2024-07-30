fbd95f55391d098cd6d2186f0cefc5d3c6a4b8cf
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-07-30 09:16:55 +0000

    Add Comment model to Django admin for management

--- comments/admin.py
+++ comments/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'post', 'created_at', 'updated_at', 'content')
+    list_filter = ('created_at', 'updated_at', 'owner')
+    search_fields = ('owner__username', 'content')
+    raw_id_fields = ('owner', 'post')
+    date_hierarchy = 'created_at'
