161e90f0f3f2792283bef8dcdab508abd486de6b
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-08-01 20:33:32 +0000

    Update REST framework settings to disable BrowsableAPIRenderer in production

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -35,16 +35,21 @@ REST_FRAMEWORK = {
             else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
         )
     ],
-    'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DATETIME_FORMAT': '%d %b %Y',
 }
-if 'DEV' not in os.environ:
+
+if 'DEV' in os.environ:
     REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
     ]
+else:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
+
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
