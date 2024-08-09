b35534c01ceb66a6b585f21aab46a004ac75ad1e
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-08-09 18:10:16 +0000

    Update profile permissions to allow public read access
    - Modified IsOwnerOrReadOnly permission to allow GET requests for all users.
    - Implemented IsAuthenticatedOrReadOnly permission for write access in ProfileDetail view.
    - Ensured only profile owners can update or delete their profiles.
    - Updated ProfileList and ProfileDetail views to reflect new permission settings.

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,16 @@
 from django.db.models import Count
 from rest_framework import generics, filters
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from .permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
+
 class ProfileList(generics.ListAPIView):
+    """
+    List all profiles. Read-only access for all users.
+    """
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
@@ -15,10 +19,18 @@ class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
     filter_backends = [filters.OrderingFilter, DjangoFilterBackend]
     filterset_fields = ['owner__following__followed__profile']
-    ordering_fields = ['posts_count', 'followers_count', 'following_count', 'owner__following__created_at', 'owner__followed__created_at']
+    ordering_fields = ['posts_count',
+                       'followers_count', 'following_count',
+                       'owner__following__created_at',
+                       'owner__followed__created_at']
+
 
 class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    """
+    Retrieve, update, or delete a profile.
+    Read access for all users, write access only for the owner.
+    """
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     queryset = Profile.objects.annotate(
         posts_count=Count('owner__post', distinct=True),
         followers_count=Count('owner__followed', distinct=True),
