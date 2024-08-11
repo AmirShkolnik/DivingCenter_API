96cb3e11629f5df2d3477e0a39c06628696ed3bb
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-08-11 08:51:45 +0000

    Profiles app - Restore files from two days ago

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import ProfileList, ProfileDetail, ProfileDeleteAPIView
+from profiles import views
 
 urlpatterns = [
-    path('profiles/', ProfileList.as_view(), name='profile-list'),
-    path('profiles/<int:pk>/', ProfileDetail.as_view(), name='profile-detail'),
-    path('profiles/<int:pk>/delete/', ProfileDeleteAPIView.as_view(), name='profile-delete'),
+    path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<int:pk>/', views.ProfileDetail.as_view()),
 ]
